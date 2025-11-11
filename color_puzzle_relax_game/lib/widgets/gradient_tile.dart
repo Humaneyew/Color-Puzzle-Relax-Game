ca8ed249ab723f70fa2d7c46f8c3599bd1b72899
UZOR <36662799+Humaneyew@users.noreply.github.com>
2025-11-12 02:15:52 +0400

    Redesign relaxing color puzzle experience

--- color_puzzle_relax_game/lib/widgets/gradient_tile.dart
+++ color_puzzle_relax_game/lib/widgets/gradient_tile.dart
@@ -18,6 +18,9 @@ class GradientTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final borderColor = isHighlighted
+        ? Colors.white.withOpacity(0.9)
+        : Colors.white.withOpacity(tile.fixed ? 0.4 : 0.25);
     return AnimatedOpacity(
       duration: const Duration(milliseconds: 200),
       opacity: dimmed ? 0.2 : 1,
@@ -28,24 +31,26 @@ class GradientTileWidget extends StatelessWidget {
         height: size,
         decoration: BoxDecoration(
           color: tile.color,
-          borderRadius: BorderRadius.circular(12),
+          borderRadius: BorderRadius.circular(14),
           border: Border.all(
-            color: isHighlighted ? Colors.white : Colors.white24,
-            width: isHighlighted ? 3 : 1,
+            color: borderColor,
+            width: isHighlighted ? 3 : 1.4,
           ),
           boxShadow: [
-            if (!tile.fixed)
-              BoxShadow(
-                color: Colors.black.withOpacity(0.08),
-                blurRadius: 12,
-                offset: const Offset(0, 4),
-              ),
+            BoxShadow(
+              color: Colors.black.withOpacity(tile.fixed ? 0.05 : 0.12),
+              blurRadius: tile.fixed ? 6 : 14,
+              offset: const Offset(0, 6),
+            ),
           ],
         ),
         child: tile.fixed
-            ? const Icon(
-                Icons.lock,
-                color: Colors.white,
+            ? Center(
+                child: Icon(
+                  Icons.close,
+                  color: Colors.white.withOpacity(0.7),
+                  size: size * 0.35,
+                ),
               )
             : null,
       ),
